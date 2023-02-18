f85de1804e92616cc51b0225e18f37dcdb643163
Yassine Bouazzati <yassine.bouazzati@etu.umontpellier.fr>
2023-02-18 19:49:35 +0100

    first commit, TODO: Creating a scene and adding the camera

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+import * as THREE from "three";
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
+renderer.render(scene, camera);
