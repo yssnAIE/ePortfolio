e4790e90a7848446a36594c8d52f12bc7fd42cb8
Yassine Bouazzati <yassine.bouazzati@etu.umontpellier.fr>
2023-02-19 14:26:47 +0100

    Ajout de la sphere

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -12,5 +13,28 @@ const camera = new THREE.PerspectiveCamera(
     window.innerWidth / window.innerHeight,
     0.1,
     1000
-)
-renderer.render(scene, camera);
+);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(50);
+scene.add(axesHelper);
+
+camera.position.set(0, 2, 5);
+orbit.update();
+
+
+
+const gridHelper = new THREE.GridHelper(20);
+scene.add(gridHelper);
+
+const sphereGeometry = new THREE.SphereGeometry(4);
+const sphereMaterial = new THREE.MeshBasicMaterial({color: 0x0000ff});
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphere);
+
+
+function animate() {    
+    renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
